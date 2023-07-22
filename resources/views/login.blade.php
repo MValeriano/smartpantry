e119c94e5dcda0682a61e2e22190345632fa66d0
marcelo.vieira <marcelo.vieira@pbh.gov.br>
2023-07-22 19:21:58 -0300

    Revertendo os estilos css

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>SmartPantry - Login</title>
-  <link href="{{ asset('css/estiloslogin.css') }}" rel="stylesheet"> 
+  <title>SmartPantry - Login</title>  
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" />
-
+  <link href="{{ asset('css/estiloslogin.css') }}" rel="stylesheet"> 
 </head>
 <body>
   <header class="header">
